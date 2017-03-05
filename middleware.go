b38f321e0f1bfba35bb7155c71e1198b6049499b
Eric Marden <eric.marden@gmail.com>
2017-03-04 19:52:45 -0600

    6: adds panic recovery middleware (#14)
    - all panics are recovered, but stack traces are only logged in
    non-production environments.
    - outputs `api.conf.Env` during startup log
    
    fixes #6

--- middleware.go
+++ middleware.go
@@ -7,8 +7,15 @@ import (
 	"github.com/gorilla/handlers"
 )
 
-// LoggingMiddleware wraps http.Handlers and outputs requests in Apache-style
+// LoggingMiddleware wraps the given http.Handler and outputs requests in Apache-style
 // Combined Log format. All logging is done to STDOUT only.
-func LoggingMiddleware(h http.Handler) http.Handler {
+func (api *API) LoggingMiddleware(h http.Handler) http.Handler {
 	return handlers.CombinedLoggingHandler(os.Stdout, h)
+}
+
+// RecoveryMiddleware wraps the given http.Handler and recovers from panics. It wil log
+// the stacktrace if HYPERDRIVE_ENVIRONMENT env var is not set to "production".
+func (api *API) RecoveryMiddleware(h http.Handler) http.Handler {
+	opt := handlers.PrintRecoveryStack(api.conf.Env != "production")
+	return handlers.RecoveryHandler(opt)(h)
 }
